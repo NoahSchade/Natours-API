5d8b5698716e13fbda5af75d12a63d9dfc89b280
Noah Schade <noahschade101@gmail.com>
2021-01-05 15:54:34 -0500

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+	app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -19,8 +19,8 @@ app.use(express.static(`${__dirname}/public`));
 // });
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+	req.requestTime = new Date().toISOString();
+	next();
 });
 
 // 3) ROUTES
