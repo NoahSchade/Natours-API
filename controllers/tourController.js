67db3584fbe915a9ca9a96ae653cff76feb4fe71
Noah Schade <noahschade101@gmail.com>
2021-01-06 16:42:59 -0500

    Fixed an error

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,7 +39,7 @@ exports.getAllTours = async (req, res) => {
 
 		query = query.skip(skip).limit(limit);
 
-		if ( req.query.page) {
+		if (req.query.page) {
 			const numTours = await Tour.countDocuments();
 			if (skip >= numTours) throw new Error('This page does not exist');
 		}
