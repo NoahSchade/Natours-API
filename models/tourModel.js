68d32f1e6008b0877fc78fbb6ba28ca7f7df788c
Noah Schade <noahschade101@gmail.com>
2021-01-16 16:39:07 -0500

    Implemented custom validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
 	{
@@ -11,6 +12,7 @@ const tourSchema = new mongoose.Schema(
 			trim: true,
 			maxlength: [40, 'A tour name must have less or equal to 40 characters'],
 			minlength: [10, 'A tour name must have more or equal to 10 characters']
+			// validate: [validator.isAlpha, 'Tour name must only contain characters']
 		},
 		slug: String,
 		duration: {
@@ -43,7 +45,16 @@ const tourSchema = new mongoose.Schema(
 			type: Number,
 			required: [true, 'A tour must have a price']
 		},
-		priceDiscount: Number,
+		priceDiscount: {
+			type: Number,
+			validate: {
+				validator: function (val) {
+					// this only points to current doc on NEW document creation
+					return val < this.price;
+				},
+				message: 'Discount price ({VALUE}) should be below regular price'
+			}
+		},
 		summary: {
 			type: String,
 			trim: true,
