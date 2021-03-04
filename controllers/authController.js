393948e5f8d4a585186f352a2287ebeba4cc4f67
Noah Schade <noahschade101@gmail.com>
2021-03-04 13:56:43 -0500

    Fixed a programming error

--- controllers/authController.js
+++ controllers/authController.js
@@ -81,8 +81,7 @@ exports.protect = catchAsync(async (req, res, next) => {
 	// 4) Check if user changed password after the token was issued
 	if (currentUser.changedPasswordAfter(decoded.iat)) {
 		return next(
-			new AppError('User recently changed password! Please log in again.'),
-			401
+			new AppError('User recently changed password! Please log in again.', 401)
 		);
 	}
 
