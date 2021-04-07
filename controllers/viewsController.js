727a10e9bc1064bc7aed456e6fc104d6085e21b6
Noah Schade <noahschade101@gmail.com>
2021-04-07 05:58:32 -0400

    Built part of the tour page.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -7,15 +7,22 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 
 	// 2) Built template
 	// 3) Render that remplate using tour data from 1)
-
 	res.status(200).render('overview', {
 		title: 'All Tours',
 		tours
 	});
 });
 
-exports.getTour = (req, res) => {
-	res.status(200).render('tour', {
-		title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res) => {
+	// 1) Get the data, for the requested tour (including reviews and guides)
+	const tour = await (await Tour.findOne({ slug: req.params.slug })).populate({
+		path: 'reviews',
+		fields: 'review rating user'
 	});
-};
+	// 2) Build template
+	// 3) Render template using data from 1)
+	res.status(200).render('tour', {
+		title: 'The Forest Hiker Tour',
+		tour
+	});
+});
