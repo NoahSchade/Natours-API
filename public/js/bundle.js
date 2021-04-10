c6d4d1f9b1cef8f8e48c5dce0300be852d2221d3
Noah Schade <noahschade101@gmail.com>
2021-04-10 10:41:16 -0400

    Implemented error pages rendering.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8608,7 +8608,7 @@ var logout = /*#__PURE__*/function () {
 
           case 3:
             res = _context2.sent;
-            if (res.data.status = 'success') window.location.replace(window.location.href);
+            if (res.data.status = 'success') location.reload(true);
             _context2.next = 10;
             break;
 
