363f407dc6006479b6cf26f08875582c6efb2ebc
Noah Schade <noahschade101@gmail.com>
2021-04-16 11:55:47 -0400

    Implemented processing payments on the front-end.

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings'
+import { bookTour } from './stripe';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -10,6 +11,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
 if (mapBox) {
@@ -47,10 +49,20 @@ if (userPasswordForm)
 		const passwordCurrent = document.getElementById('password-current').value;
 		const password = document.getElementById('password').value;
 		const passwordConfirm = document.getElementById('password-confirm').value;
-		await updateSettings({passwordCurrent, password, passwordConfirm}, 'password');
+		await updateSettings(
+			{ passwordCurrent, password, passwordConfirm },
+			'password'
+		);
 
 		document.querySelector('.btn--save-password').textContent = 'Save password';
 		document.getElementById('password-current').value = '';
 		document.getElementById('password').value = '';
 		document.getElementById('password-confirm').value = '';
 	});
+
+	if(bookBtn)
+		bookBtn.addEventListener('click', e => {
+			e.target.textContent = 'Processing...'
+			const { tourId } = e.target.dataset;
+			bookTour(tourId);
+		})
